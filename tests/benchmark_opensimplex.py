4816a6123b7056ab6f2241a49636cb27b1382aef
Thomas Rometsch <thomas.rometsch@gmail.com>
2021-05-09 19:38:38 +0200

    modify benchmark

--- tests/benchmark_opensimplex.py
+++ tests/benchmark_opensimplex.py
@@ -2,23 +2,27 @@
 import sys
 
 from opensimplex import OpenSimplex
-
-if sys.version_info[0] < 3:
-    _range = xrange
-else:
-    _range = range
+import numpy as np
 
 class Benchmark:
     def __init__(self):
         self.simplex = OpenSimplex(seed=0)
+        # trigger compilation
+        x = np.linspace(0, 1, 10)
+        self.simplex.noise2d(x, x)
+        self.simplex.noise3d(x, x, x)
+        self.simplex.noise4d(x, x, x, x)
 
-    def run(self, number=100000):
-        for i in _range(number):
-            self.simplex.noise2d(0.1, 0.1)
-            self.simplex.noise3d(0.1, 0.1, 0.1)
-            self.simplex.noise4d(0.1, 0.1, 0.1, 0.1)
+    def run(self, number=1000000):
+        x = np.linspace(0, 1, number)
+        self.simplex.noise2d(x, x)
+        self.simplex.noise3d(x, x, x)
+        self.simplex.noise4d(x, x, x, x)
 
 if __name__ == "__main__":
     import cProfile
+    import pstats
     b = Benchmark()
-    cProfile.run("b.run()", sort="cumtime")
+    cProfile.run("b.run()", "tests/opensimplex.prof")
+    ps = pstats.Stats("tests/opensimplex.prof").sort_stats(pstats.SortKey.CUMULATIVE)
+    ps.print_stats()
